df7c59f6669b577d1086e2b1af6648b92f64c015
happicat <80961808+meelkmeelk@users.noreply.github.com>
2026-02-26 20:55:30 +0800

    Create exercise2.R

---
+++ ch01-data-visualization/exercise2.R
@@ -0,0 +1,3 @@
+# What does the bill_depth_mm variable in the penguins data frame describe? Read the help for ?penguins to find out.
+?penguins
+# bill_depth_mm is a number denoting bill depth (millimeters)
